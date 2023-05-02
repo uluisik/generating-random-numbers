20308bd2533f0614656ada5d2681931fce283413
ahmet <ahmet55uluisik05@gmail.com>
2023-05-02 15:00:21 +0300

    added useState and finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import "./styless.css";
 function App() {
+  const [minVal, setMinVal] = useState(0);
+  const [maxVal, setMaxVal] = useState(10);
+  const [randomNum, setRandomNum] = useState(5);
+  const giveRandomNumber = () => {
+    setRandomNum(Math.floor(Math.random() * (maxVal - minVal + 1) + minVal));
+  };
+
   return (
     <div className="hero">
       <div className="container">
         <div className="randomNum">
           <p>
-            Random Number : <span>5</span>
+            Random Number : <span>{randomNum}</span>
           </p>
         </div>
         <div className="numContainer">
           <div>
             <p>Min:</p>
-            <input type="number" />
+            <input
+              type="number"
+              value={minVal}
+              onChange={(e) => setMinVal(+e.target.value)}
+            />
           </div>
           <div>
             <p>Max:</p>
-            <input type="number" />
+            <input
+              type="number"
+              value={maxVal}
+              onChange={(e) => setMaxVal(+e.target.value)}
+            />
           </div>
         </div>
-        <button>Get Random Number</button>
+        <button onClick={giveRandomNumber}>Get Random Number</button>
       </div>
     </div>
   );
